4493a58e154cee3220df83ff76f8c166e20c356f
Bradley Gross <bradley_gross@hotmail.com>
2017-08-01 12:17:02 -0700

    updated dist

--- Epiq.AppSupport.Button.Component/dist/SupportButtonComponent.js
+++ Epiq.AppSupport.Button.Component/dist/SupportButtonComponent.js
@@ -1,55 +1,22 @@
-'use strict';
+import React from 'react';
+import dotnetify from 'dotnetify';
+import { Button } from 'react-bootstrap';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
+class SupportButtonComponent extends React.Component {
+    constructor(props) {
+        super(props);
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _dotnetify = require('dotnetify');
-
-var _dotnetify2 = _interopRequireDefault(_dotnetify);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var SupportButtonComponent = function (_React$Component) {
-    _inherits(SupportButtonComponent, _React$Component);
-
-    function SupportButtonComponent(props) {
-        _classCallCheck(this, SupportButtonComponent);
-
-        var _this = _possibleConstructorReturn(this, (SupportButtonComponent.__proto__ || Object.getPrototypeOf(SupportButtonComponent)).call(this, props));
-
-        _dotnetify2.default.react.connect("SayHiVM", _this);
-        _this.state = { Greetings: "", ServerTime: "" };
-        return _this;
+        dotnetify.react.connect("SayHiVM", this);
+        this.state = { Greetings: "", ServerTime: "" };
     }
 
-    _createClass(SupportButtonComponent, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                null,
-                this.state.Greetings,
-                _react2.default.createElement('br', null),
-                'Server time is: ',
-                this.state.ServerTime
-            );
-        }
-    }]);
+    render() {
+        return React.createElement(
+            Button,
+            { bsStyle: 'success' },
+            'Send'
+        );
+    }
+}
 
-    return SupportButtonComponent;
-}(_react2.default.Component);
-
-exports.default = SupportButtonComponent;
+export default SupportButtonComponent;
